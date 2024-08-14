377b65050a9c700c968929768df38aba14fbe7f7
Alik <aliklyn@gmail.com>
2024-08-14 15:56:51 +0630

    Update todo list files

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -3,6 +3,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllBtn");
 
 let tasks = [];
 
@@ -35,7 +36,13 @@ function clearCompletedTasks(){
     displayTasks();
 }
 
+function clearAll(){
+    tasks = [];
+    displayTasks();
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAll);
 
 displayTasks();
